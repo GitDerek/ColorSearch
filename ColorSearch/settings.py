04706d107cde047e0c923765b8a5bc532409e4f7
Kevin Chen <me@kevchentw.com>
2015-06-28 15:20:47 +0800

    update

--- ColorSearch/settings.py
+++ ColorSearch/settings.py
@@ -20,11 +20,11 @@ BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 SECRET_KEY = 'xpzv3##2t^ngqc#%oy65@2lfqaf)yhm6mwyy+y!+uw!)rqjhe!'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 TEMPLATE_DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
